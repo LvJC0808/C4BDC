7bb30cd754fa4911c9414ed42919400890df0e76
LvJC0808 <2933173373@qq.com>
2026-04-24 12:57:26 +0800

    feat: enable MCAP_MIN_LARGE=3 for W1 submission (M10-3 selected)

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 cd /app
+export MCAP_MIN_LARGE=${MCAP_MIN_LARGE:-3}
+export MCAP_CAND_K=${MCAP_CAND_K:-10}
+export MCAP_LARGE_Q=${MCAP_LARGE_Q:-0.5}
 python scripts/predict_lgb_only.py
